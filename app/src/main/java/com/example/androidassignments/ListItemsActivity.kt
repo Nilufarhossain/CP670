41b24e89875c09d4700c8d451bd7377dfd61d0ae
Nilufarhossain <hossainnilufar12@gmail.com>
2025-05-24 15:25:28 -0400

    Added SharedPreferences for login email and login button to launch MainActivity

--- app/src/main/java/com/example/androidassignments/ListItemsActivity.kt
+++ app/src/main/java/com/example/androidassignments/ListItemsActivity.kt
@@ -6,16 +6,44 @@ import androidx.activity.enableEdgeToEdge
 import android.app.Activity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.util.Log
 
 class ListItemsActivity : Activity() {
+    private val TAG = "ListItemActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
         setContentView(R.layout.activity_list_items)
+        Log.i(TAG,  "onCreate called")
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG,  "onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG,  "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG,  "onDestroy called")
     }
 }
--- app/src/main/java/com/example/androidassignments/MainActivity.kt
+++ app/src/main/java/com/example/androidassignments/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.example.androidassignments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        Log.i(TAG, "onCreate called")
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "onDestroy called")
     }
 }
